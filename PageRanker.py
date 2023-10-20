2f04318414dbca93a6794cbefede0ef7c02adda8
Vianney Ramos <vianneyr08@gmail.com>
2023-10-19 20:59:26 -0400

    Starting mini project 4 pageranker

---
+++ PageRanker.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+PageRank Algorithm
+    -first I need to make the matrix given to become
+    a new matrix that adds up to 1. 
+    - Assume that the array A is already numpy array
+    
+"""
+import numpy as np
+
+def PageRanker(A):
+    #intitalize the array so it becomes the 
+    
+
+
