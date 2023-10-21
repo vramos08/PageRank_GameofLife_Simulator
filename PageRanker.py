a1bff14770f4f5d2082a98b288bcae0dc0cf4192
vramos08 <142256702+vramos08@users.noreply.github.com>
2023-10-20 22:54:33 -0400

    Update PageRanker.py
    For this, I made the description better, I also included the test array and calculated the real eigen value.

--- PageRanker.py
+++ PageRanker.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 """
 PageRank Algorithm
-    -first I need to make the matrix given to become
-    a new matrix that adds up to 1. 
-    - Assume that the array A is already numpy array
-    
+    This algorithm will create a matrix where the array will have a fixed
+    size of 6 by 6 with zeros and ones. If there is a one, that means that there
+    is a connection between the pages and a zero shows no connection.
+    I will normalize this array by finding the total of each
+    column and dividing each one by the sum. I will then create a vector r
+    and apply the formula r′ = αMr + (1−α)s and iterate until r converges.
 """
 import numpy as np
 
 def PageRanker(A):
     #normalize the array so that it becomes the sum of 1
-    #To do this I wil be adding each column and diving it by the total
+    #To do this I will be adding each column and diving it by the total
     total1 = 0 
     total2 = 0
     total3 = 0
@@ -19,56 +21,49 @@ def PageRanker(A):
     total6 = 0
     
     #looping through the array and adding each column 
-    for i in range (0, 5):
-        for j in range (0,5):
-            if i==0:
+    for i in range (0, 6):
+        for j in range (0,6):
+            if j==0:
                 total1 = total1+ A[i,j]
-            elif i==1:
+            elif j==1:
                 total2 = total2 + A[i,j]
-            elif i==2:
+            elif j==2:
                 total3 = total3 + A[i,j]
-            elif i==3:
+            elif j==3:
                 total4 = total4 + A[i,j]
-            elif i==4:
+            elif j==4:
                 total5 = total5 + A[i,j]
             else:
                 total6 = total6 + A[i,j]
-    #now i know the total for each column, I will be changing it so that
+    #now I know the total for each column, I will be changing it so that
     #it will become 1/total for each 1
-    
     M = np.zeros((6,6), dtype = np.float64)
     
     #I will be looping through the matrix A to find 1s and replace them
-    for i in range (0,5):
-        for j in range (0,5):
+    for i in range (0,6):
+        for j in range (0,6):
             if A[i,j]==1:
-                if i==0:
-                    newVal = 1/total1
-                    M[i,j] = newVal
-                elif i==1:
-                    newVal = 1/total2
-                    M[i,j]== newVal
-                elif i==2:
-                    newVal = 1/total3
-                    M[i,j] = newVal
-                elif i==3:
-                    newVal = 1/total4
-                    M[i,j] = 1/newVal
-                elif i==4:
-                    newVal = 1/total5
-                    M[i,j] = newVal
-                elif i==5:
-                    newVal = 1/total6
-                    M[i,j] = newVal
+                if j==0:
+                    M[i,j] = 1/total1
+                elif j==1:
+                    M[i,j] = 1/total2
+                elif j==2:
+                    M[i,j] = 1/total3
+                elif j==3:
+                    M[i,j] = 1/total4
+                elif j==4:
+                    M[i,j] = 1/total5
+                elif j==5:
+                    M[i,j] = 1/total6
     #initialize a vector named r with size 6 and each value is 1/6
     #s = vector size 6 with all entries equal to 1/6
     #threshold of 1e -6
     a = 0.85
     r = np.array([1/6,1/6,1/6,1/6,1/6,1/6], dtype = np.float64)
-    s = np.array([1/6,1/6,1/6,1/6,1/6,1/6], dtype = np.float64)
-    threshold = 1e-6
+    s = np.array([1/6,1/6,1/6,1/6,1/6,1/6], dtype = np.float64)    
+    threshold = .000001
     
-    #updating r until the difference between consecultive r values is
+    #updating r until the difference between consecutive r values is
     #below threshold
     converges = True
     while converges:
@@ -84,7 +79,6 @@ def PageRanker(A):
         else:
             r = updated_r
     return r
-
 #Testing my code for this section
 A = np.array([
     [0, 1, 1, 0, 0, 0],
@@ -96,7 +90,24 @@ A = np.array([
 ])
 
 test = PageRanker(A)
+#print(A)
+print("REAL")
 print(test)
+arrayMtest = np.array([
+    [0, 0.33333333, .5, 0  , 0, 0],
+    [0, 0  , 1  , .5, 0, 0],
+    [1, 0  , 0  , 0  , 0, 0],
+    [0, 0.33333333, 0  , 0  , .5, 0],
+    [0, 0.33333333, 0  , .5, 0, 1],
+    [0, 0  , 0  , 0  , .5, 0]
+])
+#print("this is real")
+
+#testing that the r part works
+# Calculate the eigenvalues and eigenvectors
+eigval, eigvec = np.linalg.eig(arrayMtest)
+print(eigvec)
+
 
 
     
